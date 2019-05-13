0f32809c5d5169819b0de706c4a7e01e95b597b7
Andy Freidenfelds <andy.freidenfelds@grainger.com>
2019-05-13 15:54:39 -0500

    changes logic for exists and removes unused deps

--- exists.js
+++ exists.js
@@ -2,9 +2,9 @@ const fs = require('fs')
 
 module.exports = function (path) {
   return new Promise(resolve => {
-    fs.access(path, fs.F_OK, err => {
-      if (err) return resolve(false)
-      return resolve(true)
+    fs.access(path, err => {
+      if (err) return resolve('does not exist')
+      return resolve('exists')
     })
   })
 }
